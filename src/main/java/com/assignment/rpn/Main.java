41a0b5b7438a9fe51b86a2aead763a33bebfa8a0
azeltinsh <artem.zetlinsh@gmail.com>
2021-06-09 17:00:39 +0200

    fixed output formatting

--- src/main/java/com/assignment/rpn/Main.java
+++ src/main/java/com/assignment/rpn/Main.java
@@ -2,7 +2,11 @@ package com.assignment.rpn;
 
 import com.assignment.rpn.calculator.Calculator;
 import com.assignment.rpn.calculator.CalculatorException;
+import com.assignment.rpn.calculator.OperandStack;
+import com.assignment.rpn.view.CommonStackFormatter;
+import com.assignment.rpn.view.StackFormatter;
 
+import java.math.BigDecimal;
 import java.util.Scanner;
 import java.util.Set;
 
@@ -14,13 +18,14 @@ import static java.lang.System.out;
  */
 public class Main {
 
-    private final static Set<String> exitCommands = Set.of("quit", "q", "exit");
+    private final static Set<String> exitCommands = Set.of("exit", "quit");
 
     public static void main(String[] args) {
         printBanner();
 
         Calculator calculator = new Calculator(
                 Set.of(ADD, SUBTRACT, MULTIPLY, DIVIDE, SQRT, UNDO, CLEAR));
+        StackFormatter<BigDecimal> formatter = new CommonStackFormatter(10);
 
         Scanner input = new Scanner(System.in);
         while (input.hasNextLine()) {
@@ -29,7 +34,8 @@ public class Main {
                 break;
             } else if (!nextLine.isBlank()) {
                 try {
-                    out.println("stack: " + calculator.interpret(nextLine));
+                    OperandStack<BigDecimal> result = calculator.interpret(nextLine);
+                    out.println("stack: " + formatter.format(result));
                 } catch (CalculatorException e) {
                     out.println(e.getMessage());
                 }
@@ -44,7 +50,7 @@ public class Main {
         out.println(" ----=-=------------------------------------=-=-=---");
         out.println("/   Welcome to the command-line RPN calculator!   /");
         out.println("--=-=-=------------------------------------=-=----");
-        out.println(" Enter `quit` or`exit` commands to finish the session.");
+        out.println(" Enter " + String.join(", ", exitCommands) + " commands to finish the session.");
         out.println();
     }
 }
